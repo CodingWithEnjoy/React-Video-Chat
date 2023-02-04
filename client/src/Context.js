4666a5d244243cf3b670c3655818cd496757bf3a
Coding With Enjoy <codingwithenjoy@gmail.com>
2023-02-04 11:22:55 +0330

    Update Context.js

--- client/src/Context.js
+++ client/src/Context.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
+const socket = io('https://codingwithenjoy.github.io/React-Video-Chat/');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
